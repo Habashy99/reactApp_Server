04a6f7eaf1ee5f06cdd869e23d7010612a6011dd
Abdelrahman Habashy <abdelrahmanhabashy971@gmail.com>
2024-03-31 15:12:46 +0200

    adding validation input

--- index.js
+++ index.js
@@ -26,29 +26,80 @@ app.get("/", (req, res, next) => {
 
 app.get("/tasks", (req, res, next) => {
     debug(req.method + ' ' + req.url);
-    res.send(tasks);
+    try {
+        if (tasks.length === 0) {
+            throw new Error("The task is empty")
+        }
+        res.send(tasks);
+    } catch (error) {
+        res.send(error);
+    }
 });
 
 app.post("/tasks/add", (req, res, next) => {
-    const task = req.body.task
-    tasks.push({ task });
-    res.send(`${task.taskName} has been added to the task array`);
+    debug(req.method + ' ' + req.url);
+    try {
+        const task = req.body.task
+        if (task == {}) {
+            throw new Error("error the task has no values")
+        }
+        if (task.taskName == "") {
+            throw new Error("task name should not be empty")
+        }
+        if (task.taskTime == "") {
+            throw new Error("task time should not be empty")
+        }
+        tasks.push({ task });
+
+        if (tasks.length === tasks.length + 1) {
+            throw new Error("task not added")
+        }
+        res.send(`${task.taskName} has been added to the task array`);
+    } catch (error) {
+        res.send(error);
+    }
 });
 
 app.put("/tasks/edit", (req, res, next) => {
-    const task = tasks[req.body.index];
-    task.taskName = req.body.taskName;
-    task.taskTime = req.body.taskTime;
+    debug(req.method + ' ' + req.url);
+    try {
+        if (req.body.index === undefined) {
+            throw new Error("index should not be empty")
+        }
+        const task = tasks[req.body.index];
+        if (task == {}) {
+            throw new Error("fail to fetch the selected task")
+        }
+        if (req.body.taskName === "") {
+            throw new Error("task name should not be empty")
+        }
+        if (req.body.taskTime === "") {
+            throw new Error("task time should not be empty")
+        }
+        task.taskName = req.body.taskName;
+        task.taskTime = req.body.taskTime;
 
-    res.send(`${task.taskName} with ${task.taskTime} has been changed`);
+        res.send(`${task.taskName} with ${task.taskTime} has been changed`);
+    } catch (error) {
+        res.send(error);
+    }
 })
 
 app.delete("/tasks/delete", (req, res, next) => {
-
-    const taskIndex = req.body.index
-    const deletedTask = tasks.splice(taskIndex, 1)
-
-    res.send(`${JSON.stringify(deletedTask)} has been deleted`);
+    debug(req.method + ' ' + req.url);
+    try {
+        const taskIndex = req.body.index
+        if (typeof taskIndex === "string") {
+            throw new Error("index should not be string must be number")
+        }
+        const deletedTask = tasks.splice(taskIndex, 1)
+        if (deletedTask == []) {
+            throw new Error("deleted task fail")
+        }
+        res.send(`${JSON.stringify(deletedTask)} has been deleted`);
+    } catch (error) {
+        res.send(error);
+    }
 })
 
 
